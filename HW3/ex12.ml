56c82efc07a78f4aff04b1b47fc6dfa24ca23a21
junmin-Chang <chjm219@kookmin.ac.kr>
2025-11-05 16:56:55 +0900

    ex12

--- HW3/ex12.ml
+++ HW3/ex12.ml
@@ -5,12 +5,22 @@ type ae =
   | TIMES of ae list
   | SUM of ae list
 
-let rec diff exp var =
- let rec aux acc alist = match alist with
- |[] -> acc
-  |hd::tl -> aux (acc @ diff hd) tl
-in
-  match exp with
-| VAR str -> VAR str
-| POWER (str, n) -> if n = 2 then TIMES [CONST 2; VAR str] else TIMES [CONST n; POWER (str, n-1)]
-| TIMES alist -> aux [] alist
+let rec diff (ae, s) =
+  match ae with
+  | CONST i -> CONST 0
+  | VAR v -> if v = s then CONST 1 else CONST 0
+  | POWER (v, i) -> if v = s then TIMES [ CONST i; POWER (v, i - 1) ] else CONST 0
+  | SUM aes -> SUM (List.map (fun ae -> diff (ae, s)) aes)
+  | TIMES aes ->
+    SUM
+      (List.map
+         (fun ae ->
+            let rec remains aei res =
+              match aei with
+              | hd :: tl ->
+                if Stdlib.compare ae hd == 0 then res @ tl else remains tl (res @ [ hd ])
+              | [] -> res
+            in
+            let remaining = remains aes [] in
+            TIMES (diff (ae, s) :: remaining))
+         aes)
